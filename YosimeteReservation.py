93199315085a7aaf73bb1c673b49a80f5ef9e6b4
unknown <lisantimark@gmail.com>
2019-09-29 23:01:13 -0400

    being django setup - test request with non default header

--- YosimeteReservation.py
+++ YosimeteReservation.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-result = requests.get("https://www.google.com/")
+# Set header
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
+result = requests.get("https://www.recreation.gov/camping/campgrounds/232449/availability", headers=headers)
 
 print(result.status_code)
